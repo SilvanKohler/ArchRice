fde672107296b3da56c60a17cd803115345238ea
Silvan Kohler <SilvanKohler@protonmail.com>
2021-01-22 16:52:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Login Manager
 sudo pacman -s lightdm lightdm-gtk-greeter
-# X Window
 sudo pacman -S xorg-server xorg-apps xorg-xinit
-#i3
 sudo pacman -S i3-gaps
-#alacritty
 sudo pacman -S alacritty
+sudo pacman -S rxvt-unicode
+sudo pacman -S dmenu
+sudo pacman -S vim
